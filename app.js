1b326c499fce6e4729da9506ff6a60fe4bf11e6a
George Khoury <gke@sent.com>
2023-01-03 11:43:01 -0800

    Completed 'React Components' exercise.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // TODO
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+  const [isBold, setIsBold] = useState(false);
+
+  const style = {
+    fontWeight: isBold ? 'bold' : 'normal'
+  };
+
+  return (
+    <li style={style} onMouseEnter={() => setIsBold(true)} onMouseLeave={() => setIsBold(false)}>
+      {props.tobuy}
+    </li>
+  );
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.tobuys.map((tobuy) => (
+      <GroceryListItem tobuy={tobuy} />
+    ))}
+  </ul>
+);
+
+const App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList tobuys={['kale','spinach','broccoli', 'bananas']}/>
+  </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById("app"))
